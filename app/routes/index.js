5108b72506e95f7e1159822e43dea04981e0e4fd
amamarfo94 <46829982+amamarfo94@users.noreply.github.com>
2020-08-01 23:19:38 -0400

    read schema.json and render its data

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
+
+var rawdata = fs.readFileSync('../schema.json');
+var biz = JSON.parse(rawdata);
+console.log (biz)
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', biz);
 });
 
 module.exports = router;
