8a62785ad0f030df286b69316418b52a4ebd6eb9
anlandou <avemaria.nlandou@gmail.com>
2020-08-01 23:42:46 -0400

    changed json file name to 'businesses' and added 1 more objecct to file

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var fs = require('fs');
 var router = express.Router();
 
-var rawdata = fs.readFileSync('../schema.json');
+var rawdata = fs.readFileSync('../businesses.json');
 var biz = JSON.parse(rawdata);
 console.log (biz)
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', biz);
+  res.render('index', biz[1]);
 });
 
 module.exports = router;
